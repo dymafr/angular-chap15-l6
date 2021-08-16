b60b048f11c0c5e70ea631e8effb935b370151cf
dymafr <erwan1309@hotmail.com>
2021-08-16 18:08:56 +0200

    1 file modified

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,20 +1,20 @@
-import { HttpClient } from "@angular/common/http";
-import { Component, OnInit } from "@angular/core";
-import { User } from "../user.interface";
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user.interface';
 
 @Component({
-  selector: "app-user-list",
-  templateUrl: "./user-list.component.html",
-  styleUrls: ["./user-list.component.scss"]
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent implements OnInit {
-  public users: User[];
+  public users?: User[];
 
   constructor(private httpService: HttpClient) {}
 
   ngOnInit() {
     this.httpService
-      .get<User[]>("https://restapi.fr/api/angularuser")
+      .get<User[]>('https://restapi.fr/api/angularuser')
       .subscribe(users => {
         this.users = users;
       });
